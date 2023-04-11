95d38dca9a921e94167c97b155278bab788e200b
strayacode <dallascuber@gmail.com>
2023-04-11 13:02:33 +1000

    printing strings now

--- src/kernel/x86/loader/stage1.s
+++ src/kernel/x86/loader/stage1.s
@@ -5,6 +5,10 @@ _start:
     ; disable interrupts
     cli
 
+    ; clear the screen
+    mov ax, 0x3
+    int 0x10
+
     ; initialise the segment registers to 0
     xor ax, ax
     mov ds, ax
@@ -13,13 +17,41 @@ _start:
     mov gs, ax
     mov ss, ax
 
-    mov ah, 0xe
-    mov al, 'S'
-    int 0x10
+    ; setup the stack to start right below the bootsector
+    ; 0x500-0x7c00 is considered conventional memory in x86
+    ; so we can use that
+    mov sp, 0x7c00
+
+load_stage2:
+    ; load the stage2 bootloader
+
+    mov si, disk_fail_str
+    call print_string
 
 loop:
     hlt
     jmp loop
+
+print_string:
+    ; set mode for printing characters
+    mov ah, 0xe
+
+print_character:
+    ; load next character from si into al
+    lodsb
+
+    ; finish printing when we encounter \0
+    cmp al, 0
+    je print_string_end
+
+    ; print the character and start printing next character
+    int 0x10
+    jmp print_character
+
+print_string_end:
+    ret
+
+disk_fail_str db 'Failed to read disk', 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
