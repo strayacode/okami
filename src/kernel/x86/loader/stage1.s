a19ec05fe5399f531b2393ac2aff2b5d9ce8aa88
strayacode <dallascuber@gmail.com>
2023-04-11 13:36:06 +1000

    attempt to load stage2 loader

--- src/kernel/x86/loader/stage1.s
+++ src/kernel/x86/loader/stage1.s
@@ -2,8 +2,11 @@ section .boot
 bits 16
 global _start
 _start:
-    ; disable interrupts
+    ; disable external interrupts
     cli
+
+    ; save the disk number
+    mov [disk], dl
 
     ; clear the screen
     mov ax, 0x3
@@ -24,13 +27,32 @@ _start:
 
 load_stage2:
     ; load the stage2 bootloader
+    mov ax, 0x7e0
+    mov es, ax ; target segment
+    mov ah, 0x2 ; service for reading sectors
+    mov al, STAGE2_NUM_SECTORS ; number of sectors
+    mov ch, 0x0 ; track number
+    mov cl, 0x2 ; sector number
+    mov dh, 0x0 ; head number
+    mov dl, [disk] ; type of disk (floppy = 0x00, hdd = 0x80)
+    mov bx, 0x0 ; target offset in segment
+    int 0x13
+    jc disk_load_error
 
-    mov si, disk_fail_str
+    jmp 0x7e0:0
+
+disk_load_error:
+    mov si, disk_error_str
     call print_string
 
 loop:
     hlt
     jmp loop
+
+STAGE2_NUM_SECTORS equ 1
+
+disk:
+    db 0x0
 
 print_string:
     ; set mode for printing characters
@@ -51,7 +73,7 @@ print_character:
 print_string_end:
     ret
 
-disk_fail_str db 'Failed to read disk', 0
+disk_error_str db 'Failed to read disk', 0
 
 times 510 - ($ - $$) db 0
 dw 0xaa55
---
+++ src/kernel/x86/loader/stage2.s
@@ -0,0 +1,28 @@
+stage2:
+    mov si, stage2_str
+    call print_string
+
+loop:
+    hlt
+    jmp loop
+
+print_string:
+    ; set mode for printing characters
+    mov ah, 0xe
+
+print_character:
+    ; load next character from si into al
+    lodsb
+
+    ; finish printing when we encounter \0
+    cmp al, 0
+    je print_string_end
+
+    ; print the character and start printing next character
+    int 0x10
+    jmp print_character
+
+print_string_end:
+    ret
+
+stage2_str db 'Entered stage2', 0
