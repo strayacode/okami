8814ddad18617c550f57a43d2576b08e88b6d707
strayacode <dallascuber@gmail.com>
2023-06-15 00:06:40 +1000

    push for now

--- meta/scripts/qemu-riscv64.mk
+++ meta/scripts/qemu-riscv64.mk
@@ -1,1 +1,1 @@
-QEMUFLAGS += -machine virt -cpu rv64 -m 128M -bios none -serial mon:stdio
+QEMUFLAGS += -machine virt -cpu rv64 -smp 4 -m 128M -nographic -serial mon:stdio -bios none -device virtio-rng-device -device virtio-gpu-device -device virtio-net-device -device virtio-tablet-device -device virtio-keyboard-device
--- src/kernel/riscv64/boot.s
+++ src/kernel/riscv64/boot.s
@@ -2,6 +2,28 @@
 .section .boot
 .global _start
 _start:
+    csrr t0, mhartid
+    bnez t0, 2f
+    csrw satp, zero
 
-loop:
-    j loop
+.option push
+.option norelax
+    la gp, global_pointer
+.option pop
+
+    la a0, bss_start
+    la a1, bss_end
+
+bss_init:
+    bgeu a0, a1, 1f
+    sd zero, (a0)
+    addi a0, a0, 8
+    j bss_init
+
+1:
+    la t1, arch_main
+    csrw mepc, t1
+    tail arch_main
+2:
+    wfi
+    j 2b
